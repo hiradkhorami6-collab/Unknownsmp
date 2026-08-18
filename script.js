3581f2eef9e1e9ba942b881cdb56aaae1380df22
hiradkhorami6-collab <hiradkhorami6@gmail.com>
2026-08-18 02:55:09 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const WEBHOOK_URL="YOUR_WEBHOOK_HERE";
-join.onsubmit=async(e)=>{
-e.preventDefault();
-await fetch(WEBHOOK_URL,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({content:`Join: ${mc.value} | TG: ${tg.value} | ${reason.value}`})});
-alert("Sent");
-}
